3c2556397c5d78d63952715364af1cb71a7667fc
xiften <6wn5seb9@anonaddy.me>
2022-12-25 08:35:15 -0700

    added stuff

--- src/server/trpc/router/lesson.ts
+++ src/server/trpc/router/lesson.ts
@@ -1,17 +1,19 @@
 import { TRPCError } from "@trpc/server";
+import { LessonFindManySchema } from "../../../../prisma/generated/schemas/findManyLesson.schema";
 import { LessonFindUniqueSchema } from "../../../../prisma/generated/schemas/findUniqueLesson.schema";
 import { publicProcedure, router } from "../trpc";
 
 export const lessonRouter = router({
-  findUnique: publicProcedure
-    .input(LessonFindUniqueSchema)
-    .mutation(async ({ ctx, input }) => {
-      const lesson = await ctx.prisma.lesson.findUniqueOrThrow({
-        where: input.where,
-      });
+    findUnique: publicProcedure
+        .input(LessonFindUniqueSchema)
+        .mutation(async ({ ctx, input }) => {
+            const lesson = await ctx.prisma.lesson.findUniqueOrThrow({
+                where: input.where,
+            });
 
-      if (lesson.isPremium && !ctx.session?.user?.isPremium) {
-        throw new TRPCError({ code: "UNAUTHORIZED" });
-      }
-    }),
+            if (lesson.isPremium && !ctx.session?.user?.isPremium) {
+                throw new TRPCError({ code: "UNAUTHORIZED" });
+            }
+        }),
+
 });
